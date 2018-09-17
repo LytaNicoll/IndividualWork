0cf19a9f043dc834102794a23e1d8d7131b5d747
LytaNicoll <lyta.nicoll@gmail.com>
2018-09-17 15:02:11 -0500

    variables practice

---
+++ src/outputExamples/practicewvars.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class practicewvars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int age;
+			char choiceLetter;
+			double money;
+			boolean answer;
+	}
+
+}
