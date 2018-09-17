9ecc27689b35819824bb05ad1b2dd2812e523b3b
LytaNicoll <lyta.nicoll@gmail.com>
2018-09-17 14:33:57 -0500

    College

---
+++ src/outputExamples/college.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class college {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("South Dakota Mines");
+	}
+
+}
