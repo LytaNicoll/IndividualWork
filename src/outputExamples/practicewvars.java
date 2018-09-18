7428bc598d5e84c19a7195b036d2485f02d4d590
LytaNicoll <lyta.nicoll@gmail.com>
2018-09-18 14:44:48 -0500

    Okay it is pretty much the same thing but with a real message

--- src/outputExamples/practicewvars.java
+++ src/outputExamples/practicewvars.java
@@ -4,15 +4,20 @@ public class practicewvars {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int age;
-			char choiceLetter;
-			double money;
-			boolean answer;
+			int age=3;
+			char choiceLetter='c';
+			double money=4.22;
+			boolean answer=true;
+			char descision='r';
+			
 			
 			age = 4;
-			System.out.println(age);
-			choiceLetter = 66;
-			System.out.println(choiceLetter);
+			System.out.println("Alex is " +age +" years old");
+			choiceLetter = 68;
+			System.out.println("They are in class " +choiceLetter +": Not a threat");
+			System.out.println("They have earned $" +(money+2.04)+" in their entire life");
+			System.out.println("Could a real four year old have this much money? " + answer);
+			System.out.println("Conclusion: Alex is " +descision +"eal");
 	}
 
 }
