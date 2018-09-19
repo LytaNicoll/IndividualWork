394219cc5450262b2797ff5632bd18f98bd93c8e
LytaNicoll <lyta.nicoll@gmail.com>
2018-09-19 14:52:44 -0500

    Okay it's broken but I can save it this way

--- src/outputExamples/newstuff.java
+++ src/outputExamples/newstuff.java
@@ -1,10 +1,12 @@
 package outputExamples;
-
+import java.util.Scanner;
 public class newstuff {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int age = 0;
+		Scanner input=new Scanner(System.in);
+		nextInt(input);
 	}
 
 }
