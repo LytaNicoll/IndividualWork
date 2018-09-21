78c44817331774b46598b68aac4f521afb2855b6
LytaNicoll <lyta.nicoll@gmail.com>
2018-09-21 14:21:45 -0500

    Ageoperator

---
+++ src/mathOperators/Pro.java
@@ -0,0 +1,15 @@
+package mathOperators;
+import java.util.Scanner;
+public class Pro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int init=2;
+		Scanner age=new Scanner(System.in);
+		System.out.println("How old are you?");
+		init= age.nextInt();
+		System.out.println("I see that you will be " +(init+1)+ " years old next year");
+
+	}
+
+}
