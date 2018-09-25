9e64b3da67d8dc1a4f15ca63637bde420cd0f230
LytaNicoll <lyta.nicoll@gmail.com>
2018-09-25 14:42:40 -0500

    Sphere with math

--- src/mathOperators/Sphere.java
+++ src/mathOperators/Sphere.java
@@ -1,17 +1,20 @@
 package mathOperators;
 import java.util.Scanner;
+import java.lang.Math;
 public class Sphere {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double init=2;
+		double thing=3;
+		double tingy=0;
 		final double AREA=1.3333333333333333;
 		final double PI=3.14159;
 		Scanner spherei=new Scanner(System.in);
 		System.out.println("What is the radius of the sphere?");
 		init= spherei.nextDouble();
-		System.out.println("The volume is " +((init*init*init)*AREA*PI)+ " units cubed");
+		tingy=Math.pow(init,thing);
+		System.out.println("The volume is " +((Math.PI)*(AREA)*(tingy))+ " units cubed");
 
 	}
-
 }
