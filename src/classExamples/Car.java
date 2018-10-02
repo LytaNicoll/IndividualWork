c2095834ccf25bed150758bcc543087c1d722dd8
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-02 14:18:06 -0500

    Car project
    Works multiple ways

--- src/classExamples/Car.java
+++ src/classExamples/Car.java
@@ -1,8 +1,24 @@
 package classExamples;
-
+import java.util.Scanner;
 public class Car {
 	public Car()
 	{
 		
+	}
+	public void Miles()
+	{
+		int miles=4;
+		Scanner input=new Scanner(System.in);
+		System.out.println("How many miles did you go this drive?");
+		miles=input.nextInt();
+		System.out.println(miles+3000);
+	}
+	public void Drinks()
+	{
+		System.out.println("I'm Thirsty! Feed me some gas!");
+	}
+	public void leak()
+	{
+		System.out.println("I have a leak! My tire is flat!");
 	}
 }
---
+++ src/classExamples/Leaky.java
@@ -0,0 +1,12 @@
+package classExamples;
+
+public class Leaky {
+	public Leaky()
+	{
+		
+	}
+	public void leak()
+	{
+		System.out.println("I have a leak! My tire is flat!");
+	}
+}
