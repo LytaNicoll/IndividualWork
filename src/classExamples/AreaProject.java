93bd145c7b9c3773a81df9c35d4a025b414e3379
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-03 14:32:35 -0500

    Wall

---
+++ src/classExamples/AreaProject.java
@@ -0,0 +1,16 @@
+package classExamples;
+
+public class AreaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter jenny=new Greeter();
+		Car car=new Car();
+		jenny.sayHello();
+		car.Drinks();
+		car.leak();
+		car.Miles();
+		
+	}
+
+}
--- src/classExamples/CarProject.java
+++ src/classExamples/CarProject.java
@@ -4,12 +4,10 @@ public class CarProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Greeter jenny=new Greeter();
-		Car car=new Car();
-		jenny.sayHello();
-		car.Drinks();
-		car.leak();
-		car.Miles();
+		Area wall=new Area();
+		wall.Wall();
+		wall.Door();
+	
 		
 	}
 
