e9307039abc3cfcefa4fc568aea05953f521a853
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-05 14:46:39 -0500

    Double Parameters
    with scanners as said parameters

--- src/classExamples/TestProg.java
+++ src/classExamples/TestProg.java
@@ -1,13 +1,15 @@
 package classExamples;
-
+import java.util.Scanner;
 public class TestProg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			Scanner input=new Scanner(System.in);
 			Numbers Bob=new Numbers();
 			Bob.sayNumber(7.5);
 			Bob.sayNumbertwo(3.8);
-			Bob.saySum(2.664, 4.32);
+			System.out.println("Now you give me a number!(Or two)");
+			Bob.saySum(input.nextDouble(), input.nextDouble());
 	}
 
 }
