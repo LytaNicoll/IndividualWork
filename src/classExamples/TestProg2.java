aaa520d15b527a8e11a3c17bd164aee86a99afe5
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-05 15:03:35 -0500

    More stuff

--- src/classExamples/TestProg2.java
+++ src/classExamples/TestProg2.java
@@ -1,13 +1,15 @@
 package classExamples;
 import java.util.Scanner;
-public class TestProg {
+public class TestProg2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			Scanner input=new Scanner(System.in);
 			rectangles Bob=new rectangles();
-			System.out.println("Feed me a decimal!");
-			Bob.sayRound(input.nextDouble());
+			Bob.sayNumber(7.5);
+			Bob.sayNumbertwo(3.8);
+			System.out.println("Now you give me the length and width of a rectangle");
+			Bob.sayArea(input.nextDouble(), input.nextDouble());
 	}
 
 }
