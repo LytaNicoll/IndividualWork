40beafbe87f9792f0c052e912d679b744cc17ed1
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-17 14:36:34 -0500

    Grades
    Now with math and have a nice day

--- src/Desiscions/Grades.java
+++ src/Desiscions/Grades.java
@@ -4,20 +4,23 @@ public class Grades {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int test1=1;
-		int test2=1;
+		double test1=1;
+		double test2=1;
+		double dif=1;
 		System.out.println("Score of test one?");
 		Scanner input=new Scanner(System.in);
-		test1=input.nextInt();
+		test1=input.nextDouble();
 		System.out.println("Score of test two?");
-		test2=input.nextInt();
+		test2=input.nextDouble();
 		if (test1>test2){
 			System.out.println("Wow!You enetered the tests in the right order!");
+			dif=test1-test2;
+			System.out.println("The scores are exactly "+ dif+ " points apart!");
 		}
 		else{
 			System.out.println("You dumdum");	
 		}
-		System.out.println("DundunDun");
+		System.out.println("Have a nice day!");
 		
 	}
 
