8c073b4e1b83bf2fc779506dba258de562e2ac1e
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-23 14:55:43 -0500

    Averager
    Now formatted Mr.O's way, use for future reference

--- src/Loop/DoubleNums.java
+++ src/Loop/DoubleNums.java
@@ -6,19 +6,29 @@ public class DoubleNums {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		 * int count = 0; int blue = 0; int num = 0; while (num != 9999) {
+		 * Scanner input = new Scanner(System.in);
+		 * System.out.println("Please input a number(9999 to quit)"); num =
+		 * input.nextInt(); if (num!=9999){ blue = num+blue; count++; } if (num
+		 * == 9999) { System.out.println(blue/count); } } }
+		 */
+
 		int count = 0;
 		int blue = 0;
 		int num = 0;
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please input a number(9999 to quit)");
+		num = input.nextInt();
 		while (num != 9999) {
-			Scanner input = new Scanner(System.in);
+			blue = num + blue;
+			count++;
 			System.out.println("Please input a number(9999 to quit)");
 			num = input.nextInt();
-			if (num!=9999){
-			blue = num+blue;}
-			if (num == 9999) {
-				System.out.println(blue);
-			}
 		}
+
+		System.out.println(blue / count);
 	}
 
 }
