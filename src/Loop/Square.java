e970ce1c9dc57e71bf25e6f73b2e9a7f6fb7d6f0
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-29 14:28:19 -0500

    NumSquare

---
+++ src/Loop/Square.java
@@ -0,0 +1,15 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int counter = 1; counter <= (5); counter++) {
+			for (int counter2 = 1; counter2 <= (5); counter2++) {
+				System.out.print((counter * counter2 + "   "));
+			}System.out.println();
+		}
+	}
+}
