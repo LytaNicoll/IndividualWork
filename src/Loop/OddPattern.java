be691ae52c47776cf686ded8e0418f7b009c363e
LytaNicoll <lyta.nicoll@gmail.com>
2018-10-30 14:59:45 -0500

    Number Thing
    This is definitely not how you are supposed to do it, but by golly, it works

---
+++ src/Loop/OddPattern.java
@@ -0,0 +1,30 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class OddPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * for (int counter = 1; counter <= (5); counter++) {
+		 * System.out.print((1 * counter)); }
+		 */
+		int counter = 1;
+		int counter2 = 1;
+		int j = 0;
+		while (counter <= 5) {
+			
+			counter++;
+			j++;
+			for (counter2 = j; counter2 <= (5); counter2++) {
+				System.out.print((1 * counter2));
+				}
+				
+				
+			System.out.print((counter-1));
+			System.out.println();
+
+		}
+	}
+}
