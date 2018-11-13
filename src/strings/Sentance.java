faba6965ec7e7f31afa8ca8f0321505fd8b2dc3e
Lyta <lyta.nicoll@gmail.com>
2018-11-13 14:28:57 -0600

    SentanceLength

--- src/strings/Sentance.java
+++ src/strings/Sentance.java
@@ -5,21 +5,10 @@ public class Sentance {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String sentance="none";
-		String blue="hi";
-		char sen='h';
 		Scanner input=new Scanner(System.in);
 		System.out.println("Please enter a sentance");
 		sentance = input.nextLine();
-		System.out.println("Please enter a sentance");
-		blue = input.next();
-		sen=sentance.charAt(0);
-		System.out.println(sen);
-		sen=sentance.charAt(1);
-		System.out.println(sen);
-		sen=sentance.charAt(2);
-		System.out.println(sen);
-		System.out.println(sentance.contains("bob"));
-		System.out.println(sentance);
+		System.out.println(sentance.indexOf('x')+" "+sentance.length());
 		
 	}
 
