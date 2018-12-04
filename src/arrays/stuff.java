a2d3552f16ee5239005573cbb63265f5895c9294
Lyta <lyta.nicoll@gmail.com>
2018-12-04 14:16:53 -0600

    3D array

--- src/arrays/stuff.java
+++ src/arrays/stuff.java
@@ -7,25 +7,29 @@ public class stuff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		double[][] scores = new double[7][3];
-		double[]total=new double [7];
-		double tot=0;
-		double t=0;
+		double[][][] scores = new double[7][3][9];
+		double[] total = new double[7];
+		double tot = 0;
+		double t = 0;
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 3; j++) {
-			scores[i][j] = 1 + ((int) (Math.random() * (100 - 1 + 1)));
+				for (int k = 0; k < 9; k++) {
+					scores[i][j][k] = 0 + ((int) (Math.random() * (50 - 0 + 1)));
+				}
 			}
-			
 		}
 		for (int i = 0; i < 7; i++) {
-			for (int k = 0; k < 3; k++) {
-				tot=tot+scores[i][k];
-				total[i]=scores[i][k]+tot;
-			}}
-			for (int i = 0; i < 7; i++) {
-				t=total[i]+t;
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 9; k++) {
+					tot = tot + scores[i][j][k];
+					total[i] = scores[i][j][k] + tot;
+				}
 			}
-			System.out.println(t);
-		
-	
-}}
+		}
+		for (int i = 0; i < 7; i++) {
+			t = total[i] + t;
+		}
+		System.out.println(t);
+
+	}
+}
