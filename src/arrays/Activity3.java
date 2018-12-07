dc79ce3387809aa7485adab924a3b8c4fa1f6667
Lyta <lyta.nicoll@gmail.com>
2018-12-07 15:03:10 -0600

    Set method for Array Lists

--- src/arrays/Activity3.java
+++ src/arrays/Activity3.java
@@ -8,14 +8,22 @@ public class Activity3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Rectangle> bob = new ArrayList<Rectangle>();
-		Scanner input = new Scanner(System.in);
-		for (int i = 0; i < 400; i++) {
-			Rectangle sue = new Rectangle(50+(int) (Math.random() * (70 - 50+ 1)), 50+(int) (Math.random() * (70 - 50 + 1)));
-			bob.add(0,sue);
-		}
-		for (int i = 0; i < 400; i++) {
-			System.out.println(bob.get(i).area());
+		try (Scanner input = new Scanner(System.in)) {
+			double remArea = 0;
+			for (int i = 0; i < 399; i++) {
+				Rectangle sue = new Rectangle(50 + (int) (Math.random() * (70 - 50 + 1)),
+						50 + (int) (Math.random() * (70 - 50 + 1)));
+				bob.add(sue);
+			}
+			Rectangle lee = new Rectangle(51, 60);
+			bob.add(0, lee);
+			Rectangle joe = new Rectangle();
+			joe = bob.remove(bob.size() - 1);
+			bob.set(0, joe);
+			for (int i = 0; i < bob.size(); i++) {
+				System.out.println(
+						"The rectangle with an area of " + bob.get(i).area() + " Units is rectangle number " + (i + 1));
+			}
 		}
 	}
-
 }
